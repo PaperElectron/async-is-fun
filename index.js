9541a41c352eadb8f243fadf6047316034c9a057
Jim Bulkowski <jim.b@paperelectron.com>
2018-03-23 19:35:45 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project async-is-fun
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+'use strict';
+
+/**
+ *
+ * @module index
+ */
+
+module.exports = {}
